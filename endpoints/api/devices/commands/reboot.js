5ac8ab418912922f5c6d0444d1b4901aad5aa35f
Garrett Hoofman <gambitsunob@gmail.com>
2017-12-28 18:33:31 -0500

    using id

--- endpoints/api/devices/commands/reboot.js
+++ endpoints/api/devices/commands/reboot.js
@@ -3,7 +3,7 @@ const mqtt = require('../../../../utils/mqtt.js');
 module.exports = function(app) {
     app.get('/api/v1/:id/reboot', function(req, res) {
         // TODO: (garrett) make sure the signed in user owns this device before publishing the command
-        mqtt.command('AABBCCDDEEFF', 'reboot', null);
+        mqtt.command(req.params.id, 'reboot', null);
         res.send({});
     });
 }
--- endpoints/api/devices/commands/update.js
+++ endpoints/api/devices/commands/update.js
@@ -3,7 +3,7 @@ const mqtt = require('../../../../utils/mqtt.js');
 module.exports = function(app) {
     app.get('/api/v1/:id/update', function(req, res) {
         // TODO: (garrett) make sure the signed in user owns this device before publishing the command
-        mqtt.command('AABBCCDDEEFF', 'update', null);
+        mqtt.command(req.params.id, 'update', null);
         res.send({});
     });
 }
