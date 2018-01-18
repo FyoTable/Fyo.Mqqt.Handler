e0dd2784c6448ff58847f27b726a22b00d689c81
Garrett Hoofman <gambitsunob@gmail.com>
2018-01-18 15:54:28 -0500

    connect msg forward

---
+++ endpoints/api/devices/commands/connect.js
@@ -0,0 +1,10 @@
+const mqtt = require('../../../../utils/mqtt.js');
+
+var client = mqtt.client();
+
+module.exports = function(app) {
+    app.get('/api/v1/:id/connect/:player', function(req, res) {
+        // TODO: (garrett) make sure the signed in user owns this device before publishing the command
+        mqtt.command(req.params.id, 'connecting', req.params.player);
+    });
+}
