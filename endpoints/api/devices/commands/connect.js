8520a5f30f7866adde2ba0d364d3a09d4888a535
Garrett Hoofman <gambitsunob@gmail.com>
2018-01-18 16:22:10 -0500

    response

--- endpoints/api/devices/commands/connect.js
+++ endpoints/api/devices/commands/connect.js
@@ -6,5 +6,6 @@ module.exports = function(app) {
     app.get('/api/v1/:id/connect/:player', function(req, res) {
         // TODO: (garrett) make sure the signed in user owns this device before publishing the command
         mqtt.command(req.params.id, 'connecting', req.params.player);
+        res.send('success');
     });
 }
